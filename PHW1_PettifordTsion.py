6c48e3e59b419aeafd74b500ac67289efb4fdbc7
Tsionp <146122439+Tsionp@users.noreply.github.com>
2023-09-26 14:20:08 -0400

    First upload
    input and output expressions.

---
+++ PHW1_PettifordTsion.py
@@ -0,0 +1,13 @@
+#Tsion Pettiford
+#09/26/2023
+#Input output expressions
+
+num1 = int(input("Enter a number: "))
+print("You enetered:", num1)
+print(num1, "squared is", num1**2,)
+print("And",num1, "cubed is", num1**3,"!!")
+#enter the new number and multiply and add them by each other 
+num2 = int(input("Enter another number: "))
+print(num1, "+",num2,"is", num2+num1)
+print(num1, "*",num2,"is", num2*num1)
+
