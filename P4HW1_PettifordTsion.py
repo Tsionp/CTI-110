451d2ca7e1da8e09b60a1762e827085d248e311a
Tsionp <146122439+Tsionp@users.noreply.github.com>
2023-10-31 14:50:18 -0400

    Add files via upload

---
+++ P4HW1_PettifordTsion.py
@@ -0,0 +1,34 @@
+#CTI-110
+#Tsion Pettiford
+#10/10/2023
+#P2HW2 - List
+#working with for and while loops
+
+#create 6 variables based off the user's input
+num_grades = int(input("How many grades will you enter? ")) #ask the user how many grades they are going to enter
+grade_list = [] #create empty list 
+for grade in range(num_grades):
+    this_grade = int(input("Enter a grade: "))
+    grade_list.append(this_grade)
+    print("Grades have been adeded to the list. ")
+
+    while this_grade < 0 or this_grade > 100:
+        print("Invalid grade.")
+        this_grade = int(input("Enter a grade: "))
+
+for grade in grade_list:
+    print(grade)
+                       
+#make variable average using len and calculate
+gradeav = sum(grade_list)/len(grade_list)
+
+#calculate max/min/sum/ and assign to show the user
+print(f'The highest score is:  {max(grade_list):.2f}')
+print(f'The lowest score is:  {min(grade_list):.2f}')
+print(f'The accumalative score is: {sum(grade_list):.2f}')
+print(f'The highest score is:  {max(grade_list):.2f}')
+#show user the average
+print("The average of all the grades is: ", gradeav)
+
+
+
